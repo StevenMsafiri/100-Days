2323771ceca67a5ca4133d510b2b7647bfee0990
StevenMsafiri <ianleonardmk47@gmail.com>
2024-10-21 14:57:37 +0300

    Final Commit

--- WebScraping - 01/main.py
+++ WebScraping - 01/main.py
@@ -33,19 +33,34 @@
 from bs4 import BeautifulSoup
 import requests
 
-news_response = requests.get(url="https://news.ycombinator.com/news")
+news_response = requests.get(url="https://news.ycombinator.com")
 
 web_page = news_response.text
 
 soup = BeautifulSoup(web_page, "html.parser")
 # print(soup.prettify())
 
-all_news = soup.select(selector=".titleline")
-print(all_news)
-count = 0
+all_news = soup.select(selector=".titleline > a")
 
+article_texts = []
+article_links = []
 for news in all_news:
-    print(news.getText())
-    count +=1
+    article_texts.append(news.getText())
+    article_links.append(news.get("href"))
 
-print(count)
+all_votes = soup.find_all(name = "span", class_="score")
+article_votes = []
+for news in all_votes:
+    article_votes.append(int(news.getText().split()[0]))
+
+# print(article_texts)
+# print(article_links)
+# print(article_votes)
+
+most_votes = max(article_votes)
+
+articles_dicts = [{"text": text, "link": link, "votes": votes} for text, link, votes in zip(article_texts, article_links, article_votes)]
+
+for article in articles_dicts:
+    if most_votes == article["votes"]:
+        print(f"The article with the most votes: \n {article}")
