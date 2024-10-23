a92b982b71d904bebeb60dfedc571cbbfbcac604
StevenMsafiri <ianleonardmk47@gmail.com>
2024-10-23 21:14:06 +0300

    Final Commit

---
+++ Working-with-Selenium/main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+# Initialize WebDriver with ChromeDriver
+options = webdriver.ChromeOptions()
+options.binary_location = "/snap/bin/chromium"
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
+driver.get("https://www.python.org/")
+driver.quit()
